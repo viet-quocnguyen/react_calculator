fc3ca425d70d3b3f403fe1a291456968519c21d4
qvnguyen <nguyenquocviet170898@gmail.com>
2019-02-25 13:49:13 -0500

    Updates

---
+++ precache-manifest.5091f8b44537bc11efd080adc5eba3f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2a36e1304a602d3300e",
+    "url": "/react_calculator/static/js/runtime~main.c2a36e13.js"
+  },
+  {
+    "revision": "fa14f5b442bc014aa1f1",
+    "url": "/react_calculator/static/js/main.fa14f5b4.chunk.js"
+  },
+  {
+    "revision": "38e692b3a29989f3e8a7",
+    "url": "/react_calculator/static/js/2.38e692b3.chunk.js"
+  },
+  {
+    "revision": "fa14f5b442bc014aa1f1",
+    "url": "/react_calculator/static/css/main.360f5c38.chunk.css"
+  },
+  {
+    "revision": "12179f58a286471004ed526530b30bf3",
+    "url": "/react_calculator/index.html"
+  }
+];
